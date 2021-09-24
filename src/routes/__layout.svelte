bb2bf4b134bb002aab3c6ee7e3933feab278e7d8
mani-kv <mani92kv@gmail.com>
2021-09-24 02:12:14 -0700

    updates to layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,10 +2,7 @@
     import "../styles/global.css";
 </script>
   <!-- Try some classes here -->
-<h1 class="mdtext uppercase text-indigo-500">
-    Hello People of Earth
-</h1>
-<div class="mdtext mdtext-blue mdtext-md">
-
+<div class="mdtext mdtext-blue text-gray-800 p-20">
+  
   <slot />
 </div>
