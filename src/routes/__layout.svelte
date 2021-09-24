6ced0e3ef8a73726ce7a33991e7e194677885351
mani-kv <mani92kv@gmail.com>
2021-09-24 01:58:21 -0700

    Initial Commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import "../styles/global.css";
+</script>
+  <!-- Try some classes here -->
+<h1 class="mdtext uppercase text-indigo-500">
+    Hello People of Earth
+</h1>
+<div class="mdtext mdtext-blue mdtext-md">
+
+  <slot />
+</div>
